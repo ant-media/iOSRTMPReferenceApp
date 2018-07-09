02abe95a2610814982c7aeb6700d6de466094c7e
Ogulcan <junkchorn@gmail.com>
2018-07-09 15:25:27 +0300

    Added UIApplication extension

---
+++ RTMP Ref App/Util/Extension/UIApplication.swift
@@ -0,0 +1,26 @@
+//
+//  UIApplication.swift
+//  RTMP Ref App
+//
+//  Created by Oğulcan on 9.07.2018.
+//  Copyright © 2018 AntMedia. All rights reserved.
+//
+
+import UIKit
+
+public extension UIApplication {
+    
+    public static func presentView(_ view: UIViewController) {
+        if (view.isBeingPresented) {
+            return
+        }
+        
+        let window = UIApplication.shared.keyWindow!
+        
+        if let modalVC = window.rootViewController?.presentedViewController {
+            modalVC.present(view, animated: true, completion: nil)
+        } else {
+            window.rootViewController!.present(view, animated: true, completion: nil)
+        }
+    }
+}
