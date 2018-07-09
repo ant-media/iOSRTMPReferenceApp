fb663a8903485905554444ff84737e2873839837
Ogulcan <junkchorn@gmail.com>
2018-07-09 13:55:45 +0300

    Added outlets for welcome

--- RTMP Ref App/WelcomeViewController.swift
+++ RTMP Ref App/WelcomeViewController.swift
@@ -9,6 +9,25 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
+    
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var logoTopAnchor: NSLayoutConstraint!
+    @IBOutlet weak var actionContainer: UIView! {
+        didSet {
+            self.actionContainer.alpha = 0
+        }
+    }
+    @IBOutlet weak var roomField: UITextField!
+    @IBOutlet weak var connectButton: UIButton!
+    @IBOutlet weak var serverButton: UIButton! {
+        didSet {
+//            if let server = Defaults[.server] {
+//                if (server.count > 0) {
+//                    self.serverButton.setTitle("Server ip: \(server)", for: .normal)
+//                }
+//            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
