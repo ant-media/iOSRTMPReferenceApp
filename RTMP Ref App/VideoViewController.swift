92255992051aff4794e0d5920d2b32440cbd87f6
Ogulcan <junkchorn@gmail.com>
2018-07-11 12:51:41 +0300

    Added video view controller

---
+++ RTMP Ref App/VideoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  VideoViewController.swift
+//  RTMP Ref App
+//
+//  Created by Oğulcan on 11.07.2018.
+//  Copyright © 2018 AntMedia. All rights reserved.
+//
+
+import UIKit
+
+class VideoViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("viewDidLoad")
+    }
+
+}
